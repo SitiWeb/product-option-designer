ef1ab4650f68daaeee2615c6a721bf63f94f29b0
sitiweb <administratie@sitiweb.nl>
2024-05-13 13:58:39 +0200

    fix z-index popup

--- product-option-designer.php
+++ product-option-designer.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom color selector
 Plugin URI: https://sitiweb.nl
 Description: This plugin adds custom color selector for WooCommerce products
-Version: 1.5
+Version: 1.6
 Author: Roberto van SitiWeb
 Author URI: https://sitiweb.nl
 License: GPL v2 or later
